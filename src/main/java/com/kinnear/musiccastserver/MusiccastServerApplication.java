afc891801ae15bec8f52680d981fa0d2abab5ac6
Adam Kinnear <akinnear@gmail.com>
2020-10-23 06:36:34 -0700

    Initial code

---
+++ src/main/java/com/kinnear/musiccastserver/MusiccastServerApplication.java
@@ -0,0 +1,13 @@
+package com.kinnear.musiccastserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiccastServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiccastServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinnear/musiccastserver/MusiccastServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinnear.musiccastserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiccastServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
